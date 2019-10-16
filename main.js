a2e255f03d37ef1ffbd7741569015bd2cff631cc
Mariana Molina <37163483+MarianaMolina@users.noreply.github.com>
2019-10-15 23:15:52 -0600

    js

---
+++ main.js
@@ -0,0 +1,8 @@
+/*crear una funcion que donde, al dar click en el boton de "leer mas"
+se cree una tabla que describe los detalles del dia*/
+
+let enlace_dia1 = document.querySelector("#enlace1");
+let enlace_dia2 = document.querySelector("#enlace2");
+let enlace_dia3 = document.querySelector("#enlace3");
+let enlace_dia4 = document.querySelector("#enlace4");
+let enlace_dia5 = document.querySelector("#enlace5");
